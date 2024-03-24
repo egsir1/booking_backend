1239f08de63e9eb7dee06cb4bc80e00a29738066
egsir1 <egsir111@gmail.com>
2024-03-24 16:56:58 +0900

    feat: user register rest api

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config"
-
+import userRoutes from '../src/routes/users'
 import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGODB_CONNECTION as string)
@@ -16,9 +16,7 @@ app.use(cors({
 }));
 
 
-app.get("/api/test", async (req: Request, res: Response)=> {
-    res.json({message:"Bismillah"})
-})
+app.use("/api/users", userRoutes)
 
 app.listen(7000, ()=> {
     console.log("Server is running on port 7000")
