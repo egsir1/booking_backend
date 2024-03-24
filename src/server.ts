0a0ecaf86ef7685d1a554133967bee5c67fb4f48
egsir1 <egsir111@gmail.com>
2024-03-24 18:22:57 +0900

    feat: login rest api business logic

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config"
 import userRoutes from '../src/routes/users'
+import authRoutes from '../src/routes/auth'
 import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGODB_CONNECTION as string)
@@ -17,6 +18,8 @@ app.use(cors({
 
 
 app.use("/api/users", userRoutes)
+
+app.use("/api/auth", authRoutes)
 
 app.listen(7000, ()=> {
     console.log("Server is running on port 7000")
